389b67426b3581c21327d4695f6b58eb59dc13f6
ImaniiTy <augusto.vieira@dcomp.ufs.br>
2017-01-12 14:41:10 -0300

    codigo novo

--- TrabED/Trabs/trab1.c
+++ TrabED/Trabs/trab1.c
@@ -10,97 +10,64 @@ typedef struct Reg {
 	void *prox;
 }ProdutoL;
 
-float imprimir(ProdutoL *p) {
+void lerReg(ProdutoL *reg) {
+	printf("Nome :\n");
+	fgets(reg->nomeProduto,50,stdin);
+	setbuf(stdin,NULL);
+	printf("valor :\n");
+	scanf("%f", &reg->preco);
+	setbuf(stdin,NULL);
+	printf("quantidade :\n");
+	scanf("%d", &reg->qtd);
+	setbuf(stdin,NULL);
+}
+
+void imprimirLista(ProdutoL *lista, ProdutoL *fim) {
 	ProdutoL *aux;
-	aux = p->prox;
-	float soma = 0;
-	do {
-		printf("Nome: %s     ", aux->nomeProduto);
-		printf("%0.2fRS    ", aux->preco);
-		printf("Quantidade: %d\n", aux->qtd);
-		soma += aux->preco * aux->qtd;
-		aux = aux->prox;
-
-	} while (aux != NULL);
-	return soma;
+	float soma;
+	for (aux = lista;aux != NULL;aux = aux->prox) {
+		printf("Nome: %s"
+			   "Preco: %0.2f\n"
+			   "Quantidade: %d\n\n", aux->nomeProduto, aux->preco, aux->qtd);
+		soma += aux->preco*aux->qtd;
+	}
+	printf("Soma parcial: %0.2f\n\n", soma);
 }
 
-void incluir(ProdutoL *p) {
-	ProdutoL *aux, *auxAnt;
-	char continuar;
-	float somaP;
-	do {
-		aux = (ProdutoL *) malloc(sizeof(ProdutoL));
-		printf("nome: \n");
-		scanf(" %s", aux->nomeProduto);
-		fflush(stdin);
-		printf("preco: \n");
-		scanf(" %f", &aux->preco);
-		fflush(stdin);
-		printf("quantidade: \n");
-		scanf(" %d", &aux->qtd);
-		fflush(stdin);
-		if(p->prox == NULL) {
-			aux->prox = p->prox;
-			p->prox = aux;
-		}
-		else {
-			auxAnt->prox = aux;
-			aux->prox = NULL;
-		}
-		auxAnt = aux;
-		system("cls");
-		somaP = imprimir(p);
-		printf("Soma Parcial: %0.2f\n", somaP);
-		printf("Finalizar?(S/N)\n");
-		scanf("%c", &continuar);
-		continuar = toupper(continuar);
-	} while (continuar == 'N');
-	system("cls");
-	imprimir(p);
-	printf("Valor Final: %0.2f", somaP);
-}
-
-void excluir(ProdutoL *p) {
-	ProdutoL *aux, *auxAnt;
-	aux = p;
-	printf("digitar nome do produto: \n");
-	char nome[50];
-	scanf(" %s", nome);
-	do {
-		auxAnt = aux;
-		aux = aux->prox;
-	} while(aux != NULL && strcmp(nome, aux->nomeProduto) != 0 );
-	if (aux == NULL) {
-		printf("produto nao existe \n");
+void incluirLista(ProdutoL *lista, ProdutoL *fim) {
+	ProdutoL *aux;
+	char c;
+	aux = malloc(sizeof(ProdutoL));
+	aux->prox = NULL;
+	lerReg(aux);
+	if (lista == NULL) {
+		lista = aux;
+		lista->prox = aux->prox;
+		fim = lista;
+		fim->prox = lista->prox;
 	}
 	else {
-		auxAnt->prox = aux->prox;
-		free(aux);
+		fim->prox = aux;
+		fim = aux;
 	}
+	imprimirLista(lista,fim);
+	printf("Finalizar? (S/N)\n");
+	scanf(" %c", &c);
+	getchar();
+	c = toupper(c);
+	if(c == 'N') {
+		incluirLista(lista,fim);
+	}
+}
+
+void excluirLista(ProdutoL *lista, ProdutoL *fim) {
+	char aux[50];
+	fgets(aux,50,stdin);
+	setbuf(stdin,NULL);
 }
 
 int main() {
-	ProdutoL *lista;
-	lista = malloc(sizeof(ProdutoL));
-	lista->prox = NULL;
-	int opcao;
-	do {
-		printf("Digite uma opcao: \n");
-		scanf("%d", &opcao);
-		switch (opcao) {
-			case 1:
-				incluir(lista);
-				break;
-			case 2:
-				excluir(lista);
-				break;
-			case 3:
-				break;
-			case 0:
-				break;
-		}
-	} while (opcao != 0);
-	system("pause");
+	ProdutoL *lista,*fim;
+	incluirLista(lista,fim);
 	return 0;
 }
